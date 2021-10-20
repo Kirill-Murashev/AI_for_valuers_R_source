76052c7de1d4e22bf3d65894d2903b8d888d9825
Kirill Murashev <kirill.murashev@gmail.com>
2021-10-20 21:48:51 +0300

    Update hist-plotting.r

--- Hist/hist-plotting.r
+++ Hist/hist-plotting.r
@@ -1,12 +1,54 @@
 library(tidyverse)
+library(moments)
 
-# Установить рабочую директорию
+# установить рабочую директорию
 setwd("/home/kaarlahti/TresoritDrive/Methodics/My/AI_for_valuers/R-source/AI_for_valuers_R_source/Hist/")
-# Прочитать данные, первая строка --- заголовок, разделитель колонок --- запятая, десятичный разделитель --- точка
+# прочитать данные, первая строка --- заголовок, разделитель колонок --- запятая, десятичный разделитель --- точка
 # x --- positional аргумент
 # header, sep, dec --- named
 almatyFlats <- read.csv('/home/kaarlahti/TresoritDrive/Methodics/My/AI_for_valuers/datasets/Almaty/almaty-apts-2019-1.csv', header = TRUE, sep = ",", dec = ".")
-almatyFlats$price <- as.numeric(almatyFlats$price)
-almatyFlats$price.m <- as.numeric(almatyFlats$price.m)
+
+# изменить тип объекта на более удобный и современный
 as_tibble(almatyFlats)
-hist(almatyFlats$price.m)
+
+# создать функцию для "нулевой" версии формулы Стёрджесса
+kHistSturgess0 <- function(x, na.omit = FALSE){ # создать фунццию, игнорировать пропущенные значения
+  n   <- NROW(x)                                # вычислить число n
+  ks0 = (max(x)-min(x))/(1+log2(n))             # рассчитать k
+  return(ks0)                                   # возвратить k
+}                                               # конец функции
+
+# создать функцию для первой версии формулы Стёрджесса
+kHistSturgess1 <- function(x, na.omit = FALSE){ # создать фунццию, игнорировать пропущенные значения
+  n   <- NROW(x)                                # вычислить число n
+  ks1 = (1+log2(n))                             # рассчитать k
+  return(ks1)                                   # возвратить k  
+}                                               # конец функции               
+
+# создать функцию для второй версии формулы Стёрджесса
+kHistSturgess2 <- function(x, na.omit = FALSE){ # создать фунццию, игнорировать пропущенные значения
+  n   <- NROW(x)                                # вычислить число n
+  ks2 = (1+(3.3*log10(n)))                      # рассчитать k
+  return(ks2)                                   # возвратить k  
+}                                               # конец функции
+
+# создать функцию для формулы Брукса и Каррузера
+kHistBruksKarruzer <- function(x, na.omit = FALSE){ # создать фунццию, игнорировать пропущенные значения
+  n   <- NROW(x)                                    # вычислить число n
+  kbk = 5*log10(n)                                  # рассчитать k
+  return(kbk)                                       # возвратить k  
+}                                                   # конец функции
+
+# создать функцию для формулы Хайнхольда и Гаеде
+kHistHeinholdGaede <- function(x, na.omit = FALSE){ # создать фунццию, игнорировать пропущенные значения
+  n   <- NROW(x)                                    # вычислить число n
+  khg = sqrt(n)                                     # рассчитать k
+  return(khg)                                       # возвратить k  
+}                                                   # конец функции
+
+# создать функцию для формулы Манна и Вальда
+kHistMannWald <- function(x, na.omit = FALSE){ # создать фунццию, игнорировать пропущенные значения
+  n   <- NROW(x)                               # вычислить число n
+  kmw = 4*sqrt()                                # рассчитать k
+  return(kmw)                                  # возвратить k  
+}                                              # конец функции
